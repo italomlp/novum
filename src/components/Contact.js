e25b95cf98e0017a12de6e5bbc5c01ab692cfd92
Italo Menezes <italomlporoca@hotmail.com>
2019-03-04 14:09:57 -0300

    add netlify flag in contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ const Contact = props => (
   <section id="contact">
     <div className="inner">
       <section>
-        <form method="post" action="#">
+        <form method="post" netlify>
           <div className="field half first">
             <label htmlFor="name">Nome</label>
             <input type="text" name="name" id="name" />
